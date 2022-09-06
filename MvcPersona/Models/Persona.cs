034e1e4800a6dca5dffa8b4222d5bd9a214b217a
Giselle-Ch <giselle.chipagua@gmail.com>
2022-09-06 15:59:25 -0600

    adicion de base de datos

---
+++ MvcPersona/Models/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcPersona.Models
+{
+    public class Persona
+    {
+        public int ID { get; set; }
+        public string DUI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        [Display(Name = "Fecha de nacimiento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Correo { get; set; }
+    }
+    public class PersonaDBContext : DbContext
+    {
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
